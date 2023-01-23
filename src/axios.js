59ff8d05cb2fe60489fa6638abf4b88328728d88
jitender joshi <jjoshi247@gmail.com>
2023-01-23 17:44:48 +0530

    build changes

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL:
+    "https://ticket-tracker-server-3qz11svs8-jjoshi247-gmailcom.vercel.app/api/v1/",
 });
 
 export default instance;
